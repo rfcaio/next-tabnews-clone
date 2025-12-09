0705e5dfd764e17fa800d347c53d3decf0194979
Caio Rodrigues <rfcaio90@gmail.com>
2025-12-09 00:00:06 -0300

    setup the `PostgreSQL` database configuration (#7)

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-const status = (_, res) =>
-  res.status(200).json({ message: 'Welcome to TabNews!' })
+import database from '../../../../infra/database'
+
+const status = async (_, res) => {
+  await database.query('SELECT 1 + 1;')
+
+  return res.status(200).json({ message: 'Welcome to TabNews!' })
+}
 
 export default status
