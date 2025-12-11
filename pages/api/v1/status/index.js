1cfd672c80afdf67e60345d7c6acbecb15b28f7e
Caio Rodrigues <rfcaio90@gmail.com>
2025-12-11 09:53:48 -0300

    update `/api/v1/status` endpoint (#9)

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,34 @@
 import database from 'infra/database'
 
 const status = async (_, res) => {
-  await database.query('SELECT 1 + 1;')
+  const updatedAt = new Date().toISOString()
 
-  return res.status(200).json({ message: 'Welcome to TabNews!' })
+  const databaseActiveConnectionsQueryResult = await database.query({
+    text: 'SELECT count(*)::int AS active_connections FROM pg_stat_activity WHERE datname = $1;',
+    values: [process.env.POSTGRES_DB]
+  })
+  const [{ active_connections }] = databaseActiveConnectionsQueryResult.rows
+
+  const databaseMaxConnectionsQueryResult = await database.query(
+    "SELECT current_setting('max_connections')::int AS max_connections;"
+  )
+  const [{ max_connections }] = databaseMaxConnectionsQueryResult.rows
+
+  const databaseVersionQueryResult = await database.query(
+    "SELECT current_setting('server_version') AS version;"
+  )
+  const [{ version }] = databaseVersionQueryResult.rows
+
+  return res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        active_connections,
+        max_connections,
+        version
+      }
+    }
+  })
 }
 
 export default status
--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,5 +1,12 @@
-test('when GET to api/v1/status then return 200', async () => {
+test('when GET to api/v1/status then returns 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status')
 
   expect(response.status).toBe(200)
+
+  const body = await response.json()
+
+  expect(body.updated_at).toBe(new Date(body.updated_at).toISOString())
+  expect(body.dependencies.database.active_connections).toBe(1)
+  expect(body.dependencies.database.max_connections).toBe(100)
+  expect(body.dependencies.database.version).toBe('16.0')
 })
