3badcdadbedafb6e784d010607eb35383d34a494
Caio Rodrigues <rfcaio90@gmail.com>
2025-12-11 12:45:07 -0300

    enable `SSL` for database connections (#10)

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,8 @@ const query = async (queryObject) => {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV !== 'development'
   })
 
   await client.connect()
